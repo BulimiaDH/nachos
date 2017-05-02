148ca651927b761a3ea2caa2d2304f4cc7fea225
MelodyLC <dearonion@foxmail.com>
2017-05-01 19:06:53 -0700

    try Communicator

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -14,7 +14,11 @@ public class Communicator {
 	 * Allocate a new communicator.
 	 */
 	public Communicator() {
-		int word;
+		this.cLock = new Lock();
+		this.Speaker = new Condition(this.cLock);
+		this.Listener = new Condition(this.cLock);
+		this.Ack = new Condition(this.cLock);
+
 	}
 
 	/**
@@ -28,18 +32,16 @@ public class Communicator {
 	 * @param word the integer to transfer.
 	 */
 	public void speak(int word) {
-		/*
-		while(somebodyIsSpeaking){
-			block myself
-		} 
-		someoneIsSpeaking = true;
+		this.cLock.acquire();
+		while(this.someoneIsSpeaking)
+			this.Speaker.sleep();
+		this.someoneIsSpeaking = true;
 		this.word  = word;
-		wake up a listener if they are waiting for someone to speak
-		wait until a listener has acknowledge they have heard my word
-		*/
-
+		this.Listener.wake();
+        this.Ack.sleep();
+        this.cLock.release();
+		
 	}
-
 	/**
 	 * Wait for a thread to speak through this communicator, and then return the
 	 * <i>word</i> that thread passed to <tt>speak()</tt>.
@@ -47,17 +49,18 @@ public class Communicator {
 	 * @return the integer transferred.
 	 */
 	public int listen() {
-		
-		// if (!someoneIsSpeaking){
-		// 	wake up someone who is waiting to speak
-		// 	wait until someone is someone
-		// }
-		// int heardWord = this.word;
-		// someoneIsSpeaking = false;
-		// acknowledge to the speaker that you've heard them
-		// return the heardwrod;
-		return 0;
-
+		this.cLock.acquire();
+		while (!this.someoneIsSpeaking)
+			this.Listener.sleep();
+		int word = this.word;
+		this.someoneIsSpeaking = false;
+		this.Ack.wake();
+		this.Speaker.wake();
+		this.cLock.release();
+		return word;
 	}
 	boolean someoneIsSpeaking = false;
+	int word;
+	Lock cLock;
+	Condition Speaker, Listener, Ack;
 }
