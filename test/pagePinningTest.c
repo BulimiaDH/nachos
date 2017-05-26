758bfc32ebb6b760fe70ac933ab6a11d3c9266f8
MelodyLC <dearonion@foxmail.com>
2017-05-25 20:45:31 -0700

    add some test files

--- test/pagePinningTest.c
+++ test/pagePinningTest.c
@@ -15,19 +15,19 @@ int main()
    int cpids [numChildren];
    int i = 0;
    for (; i < numChildren; i++) {
-//        printf("Execing child #%d ...\n",i);
+       printf("Execing child #%d ...\n",i);
         cpids[i] = exec("hello.coff", 0, 0);
    }
-//   int* status;
-//   i = 0;
-//   for (; i < numChildren; i++) {
-//        int joinRes = join(cpids[i], status);
-//        if (joinRes != 1) {
-//            printf("Joining child #%d error! status: %d\n", i+1, joinRes);
-//        }
-//    }
-//
-//    printf("End of pagePinningTest\n");
+  int* status;
+  i = 0;
+  for (; i < numChildren; i++) {
+       int joinRes = join(cpids[i], status);
+       if (joinRes != 1) {
+           printf("Joining child #%d error! status: %d\n", i+1, joinRes);
+       }
+   }
+
+   printf("End of pagePinningTest\n");
 
    return -1000;
 }
